9f364e3af95760adbd47fc516e1b6abd84e11edd
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-05-14 16:21:45 +0100

    Add & update products in cart

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,2 +1,29 @@
 class OrderItemsController < ApplicationController
+
+  def create
+    # Find product
+    @product = Product.find(params[:product_id])
+
+    @quantity = form_params[:quantity]
+
+    @current_cart.order_items.create(product: @product, quantity: @quantity)
+
+    flash[:success] = "Thanks for adding to your cart"
+
+    redirect_to product_path(@product)
+  end
+
+  def update
+    @product = Product.find(params[:product_id])
+
+    flash[:success] = "Thanks for updating to your cart"
+
+    redirect_to product_path(@product)
+  end
+
+  def form_params
+    params.require(:order_item).permit(:quantity)
+  end
+
+
 end
