88d5ccedd198191207b6a45b899b19fcaff7181f
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-05-24 20:44:16 +0100

    Enable Stripe API key

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,10 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+  # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
+  # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
+  config.require_master_key = true
+
   # Show full error reports.
   config.consider_all_requests_local = true
 
