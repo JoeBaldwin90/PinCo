3d092de786d818e99e33791432305753758e5a5c
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-05-24 19:49:47 +0100

    Improve admin order form

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -1,5 +1,49 @@
 ActiveAdmin.register Order do
 
-  permit_params :first_name, :last_name, :email,
-   :address_1, :address_2, :city, :country, :postal_code
+  permit_params :first_name,
+                :last_name,
+                :email,
+                :address_1,
+                :address_2,
+                :city,
+                :postal_code,
+                :country,
+                order_items_attributes: [:id, :quantity, :product_id]
+
+   index do
+     selectable_column
+     id_column
+     column :name do |order|
+       link_to order.first_name + " " + order.last_name, order_path(order)
+     end
+     column :country
+     column :created_at
+     actions dropdown: true
+   end
+
+   form do |f|
+
+     f.inputs do
+       f.input :first_name
+       f.input :last_name
+       f.input :email
+     end
+
+     f.inputs "Address" do
+       f.input :address_1
+       f.input :address_2
+       f.input :city
+       f.input :postal_code
+       f.input :country
+     end
+
+     f.has_many :order_items, heading: "Items" do |itemf|
+       itemf.input :product
+       itemf.input :quantity
+     end
+
+    f.actions
+  end
+
+
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -8,11 +8,15 @@ class Order < ApplicationRecord
   validates :city, presence: true
   validates :country, presence: true
 
+  accepts_nested_attributes_for :order_items
+
   def add_from_cart(cart) # pass in new variable named cart as an arguement
+
     cart.order_items.all.each do |item|
       self.order_items.new(product: item.product, # self is an Order record for each cart variable, which has_many order_items (e.g. Order.order_items)
          quantity: item.quantity) # the order_items schema shows that order_items can have a product association and a quantity
     end
+
   end
 
 end
