b76e496818255ef0f12f13538c93812f5f2359dc
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-05-24 15:16:40 +0100

    Beautification

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
 
 
   def home
-    @featured_products = Product.where(is_featured: true)
+    @featured_products = Product.where(is_featured: true).order(created_at: :desc)
   end
 
   def info
