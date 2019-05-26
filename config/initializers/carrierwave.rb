0cf22b9b8f3193ab3df11c2d15051e52e30cae9c
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-05-26 11:23:10 +0100

    Setup database and carrierwave for Heroku

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,9 @@
+CarrierWave.configure do |config|
+  config.fog_provider = 'fog/aws'                        # required
+  config.fog_credentials = {
+    provider:              'AWS',                        # required
+    aws_access_key_id:     Rails.application.credentials.aws_key,      # required unless using use_iam_profile
+    aws_secret_access_key: Rails.application.credentials.aws_password, # required unless using use_iam_profile
+  }
+  config.fog_directory  = Rails.appliication.credentials.aws_bucket                 # required
+end
