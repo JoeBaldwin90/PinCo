a5b6ae4a41f1644de2b1ffd15db7c8084eaedaec
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-05-26 11:06:50 +0100

    Functioning SendGrid code mailer.

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,8 +1,11 @@
 class OrderMailer < ApplicationMailer
 
+  default :from => 'jcameronbaldwin@gmail.com'
+
   def receipt(order)
     @order = order # Enable orders to be used in Views
 
-    mail to: @order.email, subject: "Thank you for ordering from PinCo"
+    mail( :to => @order.email,
+          :subject => "Thank you for ordering from PinCo")
   end
 end
